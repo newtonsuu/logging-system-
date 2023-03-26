2619d6b3ea423554c2824b06300bf9742d8a6b34
hackerrank <me@hackerrank.com>
2023-03-27 00:05:14 +0800

    .

--- client-ui/index.php
+++ client-ui/index.php
@@ -65,6 +65,13 @@
 					case "end":
 						location.reload();
 						break;
+					case "done":
+						if(current_step == 15){
+							current_step = 16;
+							document.querySelector(".form_3_btns .btn_done").click();
+							document.querySelector("#instruction").innerHTML = "Thank you! ";
+						}
+						break;
 					case "next":
 						if(current_step == 1){
 							current_step = 2;
@@ -83,6 +90,12 @@
 							document.querySelector(".form_2_btns .btn_back").click();
 							document.querySelector("#instruction").innerHTML = "Started form fill up. Say 'next' to proceed.";
 						}
+						if(current_step == 15){
+							current_step = 2;
+							document.querySelector(".form_3_btns .btn_back").click();
+							document.querySelector("#instruction").innerHTML = "Please, say your address.";
+						}
+						break;
 					case "yes":
 						if(current_step == 3){
 							current_step = 4;
@@ -99,10 +112,20 @@
 							document.querySelector("#departed").value = current_input_value;
 							document.querySelector("#instruction").innerHTML = "Please, Say 'next' to proceed or say 'back' to return";
 						}
-						if(current_step == 9){
-							current_step = 10;
+						else if(current_step == 10){
+							current_step = 11;
 							document.querySelector("#age").value = current_input_value;
 							document.querySelector("#instruction").innerHTML = "Please, say your firstname.";
+						}
+						else if(current_step == 12){
+							current_step = 13;
+							document.querySelector("#firstname").value = current_input_value;
+							document.querySelector("#instruction").innerHTML = "Please, say your lastname.";
+						}
+						else if(current_step == 14){
+							current_step = 15;
+							document.querySelector("#lastname").value = current_input_value;
+							document.querySelector("#instruction").innerHTML = "Please, Say 'done' to proceed or say 'back' to return";
 						}
 						break;
 					case "no":
@@ -118,22 +141,33 @@
 							current_step = 6;
 							document.querySelector("#instruction").innerHTML = "Please, say your time of departure.";
 						}
-						else if(current_step == 9){
-							current_step = 8;
+						else if(current_step == 10){
+							current_step = 9;
 							document.querySelector("#instruction").innerHTML = "Please, say your age.";
+						}
+						else if(current_step == 12){
+							current_step = 11;
+							document.querySelector("#instruction").innerHTML = "Please, say your firstname.";
+						}
+						else if(current_step == 14){
+							current_step = 13;
+							document.querySelector("#instruction").innerHTML = "Please, say your lastname.";
 						}
 						break;
 					
-					case "hello":
-						document.querySelector("#response_speech").innerHTML = "Hi!";
-						break;
+					// case "hello":
+					// 	document.querySelector("#response_speech").innerHTML = "Hi!";
+					// 	break;
 					case "turn blue":
-						document.querySelector("#response_speech").innerHTML = "changing background to blue";
-						document.body.style.backgroundColor = "blue";
+						// document.querySelector("#response_speech").innerHTML = "changing background to blue";
+						document.body.style.backgroundColor = "rgba(113, 218, 215, 1)";
+						break;
+					case "turn red":
+						document.body.style.backgroundColor = "#e75a5a";
 						break;
 					case "turn green":
-						document.querySelector("#response_speech").innerHTML = "changing background to green";
-						document.body.style.backgroundColor = "green";
+						// document.querySelector("#response_speech").innerHTML = "changing background to green";
+						document.body.style.backgroundColor = "hsla(87, 75%, 63%, 1)";
 						break;
 					default:
 						console.log("command not recognized: " + recognized_speech);
@@ -159,6 +193,18 @@
 							current_step = 10;
 							current_input_value = recognized_speech;
 							document.querySelector("#instruction").innerHTML = "Your Age is " +
+								current_input_value + ". Please say 'yes' to proceed and 'no' to retry.";
+						}
+						if(current_step == 11){
+							current_step = 12;
+							current_input_value = recognized_speech;
+							document.querySelector("#instruction").innerHTML = "Your firstname is " +
+								current_input_value + ". Please say 'yes' to proceed and 'no' to retry.";
+						}
+						if(current_step == 13){
+							current_step = 14;
+							current_input_value = recognized_speech;
+							document.querySelector("#instruction").innerHTML = "Your lastname is " +
 								current_input_value + ". Please say 'yes' to proceed and 'no' to retry.";
 						}
 				}
@@ -194,12 +240,10 @@
 		</div>
 		<div class="form_wrap">
 			<div class="form_1 data_info">
-				<h2>Personal Info</h2>
+				<h2>General-Purpose Logging System Automation</h2>
 				<form>
-					<!-- <div class="form_container">
-						<div class="input_wrap">
-						questionaire
-						</div> -->
+					<div class="form_container">
+						<div class="input_wrap">Welcome!</div>
 						<!-- <div id="a1" class="input_wrap">
 						Answers here
 						</div> -->
@@ -246,12 +290,12 @@
 								<input type="text" name="Age" class="input" id="age">
 							</div>
 							<div class="input_wrap">
-								<label for="experience">First Name</label>
-								<input type="text" name="Total Experience" class="input" id="experience">
+								<label for="firstname">First Name</label>
+								<input type="text" name="First Name" class="input" id="firstname">
 							</div>
 							<div class="input_wrap">
-								<label for="designation">Last Name</label>
-								<input type="text" name="Designation" class="input" id="designation">
+								<label for="lastname">Last Name</label>
+								<input type="text" name="Last Name" class="input" id="lastname">
 							</div>
 						</div>
 					</form>
@@ -274,7 +318,7 @@
 					<button type="button" class="btn_done">Done</button>
 				</div>
 			</div>
-			<div class= "footer"> 
+			<div class= "footer">
 				<div id="instruction">
 					Say 'start' to start fill up form
 				</div>
